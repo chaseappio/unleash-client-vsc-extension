ea45d7da0c6ff66c99b7e8994132e1e67c82718e
itay itzhaki <itay@itays-MBP-2.lan>
2022-09-19 13:44:10 +0300

    fix: extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,168 +1,173 @@
-import { randomUUID } from 'crypto';
-import { isText } from 'istextorbinary';
-import fetch from 'node-fetch'
-import * as vscode from 'vscode';
+import { isText } from "istextorbinary";
+import fetch from "node-fetch";
+import * as vscode from "vscode";
 
-const ENDPOINT = 'https://app.unleash.team';
+const ENDPOINT = "https://app.unleash.so";
 
 export function activate(context: vscode.ExtensionContext) {
+  const provider = new UnleashViewProvider(context);
 
+  // FOR TESTING since we have log off:
+  //context.globalState.update("session", null);
 
-	const provider = new UnleashViewProvider(context);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      UnleashViewProvider.viewType,
+      provider,
+      { webviewOptions: { retainContextWhenHidden: true } }
+    )
+  );
 
-	// FOR TESTING since we have log off:
-	// context.globalState.update('session',null);
+  context.subscriptions.push(
+    vscode.window.registerUriHandler(new UnleashUriHandler(context))
+  );
 
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(UnleashViewProvider.viewType, provider,{webviewOptions:{retainContextWhenHidden:true}}));
-
-	
-		context.subscriptions.push(vscode.window.registerUriHandler(new UnleashUriHandler(context)));	
-
-
-	
-	context.subscriptions.push(vscode.commands.registerCommand('unleash.search',c=>{
-		vscode.commands.executeCommand("unleash-search.focus")
-	}));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("unleash.search", (c) => {
+      vscode.commands.executeCommand("unleash-search.focus");
+    })
+  );
 }
 
-
-class UnleashUriHandler implements vscode.UriHandler{
-	
-	constructor(private context: vscode.ExtensionContext)
-	{
-
-	}
-	handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
-		let token = uri.query.split('token=')[1];
-		let s = JSON.parse(unescape( token));
-		this.context.globalState.update('session', s);
-		UnleashViewProvider._view?.webview.postMessage({type:'unleash:vsc:session',session:s});
-	}
-
+class UnleashUriHandler implements vscode.UriHandler {
+  constructor(private context: vscode.ExtensionContext) { }
+  handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
+    let token = uri.query.split("token=")[1];
+    let s = JSON.parse(unescape(token));
+    this.context.globalState.update("session", s);
+    UnleashViewProvider._view?.webview.postMessage({
+      type: "unleash:vsc:session",
+      session: s,
+    });
+  }
 }
 class UnleashViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "unleash-search";
 
-	public static readonly viewType = 'unleash-search';
-
-	public static _view?: vscode.WebviewView;
-
-	constructor(
-		private readonly _extension: vscode.ExtensionContext,
-	) { }
-
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		UnleashViewProvider._view = webviewView;
-
-		webviewView.webview.options = {
-			// Allow scripts in the webview
-			enableScripts: true,
-
-			localResourceRoots: [
-				this._extension.extensionUri
-			]
-		};
-
-		webviewView.webview.onDidReceiveMessage(async data => {
-			switch (data.type) {
-				
-				case 'unleash:vsc:openurl':
-					{						
-						if ( data.downloadUrl ) 
-						{
-
-							
-							vscode.window.withProgress({location:vscode.ProgressLocation.Notification,cancellable:true,title:'Downloading file...'},async p=>{
-								try  {
-						
-								let resp = await fetch(data.downloadUrl);
-								if ( !resp.ok)
-								{
-									vscode.window.showErrorMessage('Failed to download file: ' + resp.status);
-									return;
-								}						
-								
-								let ext=  new URL(data.url);
-								let fname = ext.pathname.split('/').reverse()[0];
-								const buf = await resp.buffer();						
-								const hash = require('crypto').createHash('sha256').update(buf).digest('hex');
-								const ldir = require('os').tmpdir()+'/'+hash+'/';
-								
-								await require('fs').promises.mkdir(ldir,{recursive:true});
-		
-								const disposition = resp.headers.get('content-disposition');
-								if ( disposition)
-								{
-									var filename = "";
-									if (disposition && disposition.indexOf('attachment') !== -1) {
-										var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-										var matches = filenameRegex.exec(disposition);
-										if (matches != null && matches[1]) { 
-										  filename = matches[1].replace(/['"]/g, '');
-										}
-									}
-									if (filename?.startsWith('UTF-8'))
-										filename=filename.substring(5);
-
-									if ( filename)
-										fname=filename;
-								}
-		
-								const lfilename = ldir+'/'+fname;						
-								await require('fs').promises.writeFile(lfilename,buf);
-		
-								if ( isText(lfilename))
-								{
-									const op = await vscode.workspace.openTextDocument(lfilename);									
-									vscode.window.showTextDocument(op);
-								}
-								else 
-								{
-									vscode.env.openExternal(vscode.Uri.parse('file://'+lfilename));
-								}
-							}
-							catch(e)
-							{
-								vscode.window.showErrorMessage('Failed to download file: ' + e);								
-							}
-							});
-
-							return;							
-						}
-						else 
-						{
-							vscode.env.openExternal( vscode.Uri.parse( data.url ) );
-						}
-
-						return;
-					}
-				case 'unleash:vsc:signin':
-					{
-						vscode.env.openExternal( vscode.Uri.parse( ENDPOINT+'/desktop?target=' + vscode.env.uriScheme ) );
-						return;
-					}
-
-				case 'unleash:vsc:init':
-					{
-						webviewView.webview.postMessage({type:'unleash:vsc:session',session:await this._extension.globalState.get('session')})
-						return;
-					}
-			}
-		});
-		
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-	
-	}
+  public static _view?: vscode.WebviewView;
 
 
-	private _getHtmlForWebview(webview: vscode.Webview) {
 
-		return `<!DOCTYPE html>
+
+  constructor(private readonly _extension: vscode.ExtensionContext) { }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    UnleashViewProvider._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extension.extensionUri],
+    };
+
+    webviewView.webview.onDidReceiveMessage(async (data) => {
+      switch (data.type) {
+        case "unleash:vsc:openurl": {
+          if (data.downloadUrl) {
+            vscode.window.withProgress(
+              {
+                location: vscode.ProgressLocation.Notification,
+                cancellable: true,
+                title: "Downloading file...",
+              },
+              async (p) => {
+                try {
+                  let resp = await fetch(data.downloadUrl);
+                  if (!resp.ok) {
+                    vscode.window.showErrorMessage(
+                      "Failed to download file: " + resp.status
+                    );
+                    return;
+                  }
+
+                  let ext = new URL(data.url);
+                  let fname = ext.pathname.split("/").reverse()[0];
+                  const buf = await resp.buffer();
+                  const hash = require("crypto")
+                    .createHash("sha256")
+                    .update(buf)
+                    .digest("hex");
+                  const ldir = require("os").tmpdir() + "/" + hash + "/";
+
+                  await require("fs").promises.mkdir(ldir, { recursive: true });
+
+                  const disposition = resp.headers.get("content-disposition");
+                  if (disposition) {
+                    var filename = "";
+                    if (
+                      disposition &&
+                      disposition.indexOf("attachment") !== -1
+                    ) {
+                      var filenameRegex =
+                        /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                      var matches = filenameRegex.exec(disposition);
+                      if (matches != null && matches[1]) {
+                        filename = matches[1].replace(/['"]/g, "");
+                      }
+                    }
+                    if (filename?.startsWith("UTF-8"))
+                      filename = filename.substring(5);
+
+                    if (filename) fname = filename;
+                  }
+
+                  const lfilename = ldir + "/" + fname;
+                  await require("fs").promises.writeFile(lfilename, buf);
+
+                  if (isText(lfilename)) {
+                    const op = await vscode.workspace.openTextDocument(
+                      lfilename
+                    );
+                    vscode.window.showTextDocument(op);
+                  } else {
+                    vscode.env.openExternal(
+                      vscode.Uri.parse("file://" + lfilename)
+                    );
+                  }
+                } catch (e) {
+                  vscode.window.showErrorMessage(
+                    "Failed to download file: " + e
+                  );
+                }
+              }
+            );
+
+            return;
+          } else {
+            vscode.env.openExternal(vscode.Uri.parse(data.url));
+          }
+
+          return;
+        }
+        case "unleash:vsc:signin": {
+          vscode.env.openExternal(
+            vscode.Uri.parse(
+              ENDPOINT + "/desktop?target=" + vscode.env.uriScheme
+            )
+          );
+          return;
+        }
+
+        case "unleash:vsc:init": {
+          webviewView.webview.postMessage({
+            type: "unleash:vsc:session",
+            session: await this._extension.globalState.get("session"),
+          });
+          return;
+        }
+      }
+    });
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 			
@@ -290,5 +295,5 @@ class UnleashViewProvider implements vscode.WebviewViewProvider {
 
 			</body>
 			</html>`;
-	}
+  }
 }
